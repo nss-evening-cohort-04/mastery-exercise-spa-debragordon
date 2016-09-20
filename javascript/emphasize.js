5ed99c49f9697d0f98cb5b0b3a7dcb5bc9b0eda6
Debra Gordon <debra.m.gordon@gmail.com>
2016-09-20 15:42:58 -0500

    I have it functioning in the MAIN.JS. Not sure how to pull into IIFEs and then acces from the main.js

---
+++ javascript/emphasize.js
@@ -0,0 +1,8 @@
+// Create one global variable (e.g. CarLot) and use the IIFE pattern to augment it two times in separate JavaScript files.
+// The final IIFE should augment the object with two more functions. One function resets the border thickness and background color for each car element back to the original values. The other function changes the thickness of the border of a car element, and changes its background color. The function must accept two arguments.
+////////// A car DOM element that was clicked on.
+////////// A color name of your choice (see behavior requirement 5 above).
+
+var CarLot = (function () {
+
+})(CarLot || {});
---
+++ javascript/events.js
@@ -0,0 +1,6 @@
+// Create one global variable (e.g. CarLot) and use the IIFE pattern to augment it two times in separate JavaScript files.
+// The second IIFE should augment the original one with a function that creates all of the eventHandlers that you need for the application. Name the function activateEvents.
+
+var CarLot = (function () {
+
+})(CarLot || {});
